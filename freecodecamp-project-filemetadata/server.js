626c8c3469ff0cb1362076876d0e62bcebf6701f
Faith-Nchifor <52848633+Faith-Nchifor@users.noreply.github.com>
2020-11-18 22:29:26 +0100

    Add files via upload

---
+++ freecodecamp-project-filemetadata/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer');
+var upload = multer({dest:'./images/'})
+
+
+
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+// create an endpoint to save the file
+// and i create a post api to handle th incomming file
+
+app.post('/api/fileanalyse',upload.single('upfile'), function(req,res){
+  try {
+    res.json(
+      {
+        'name':req.file.originalname,
+        'type': req.file.mimetype,
+        'size':req.file.size
+      }
+      )
+  }catch(error){
+    console.log({"error":error})
+  }
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
